50cc6efb31356205eb15b5d833bb11ba7c01d6dd
Arthur Schwaiger <arthur.schwaiger@gmail.com>
2015-07-17 16:45:42 +0200

    Add Grunt +  express-server plugni (with default dev server) new task: grunt server:dev

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+/**
+ * Grunt config
+ */
+module.exports = function(grunt) {
+	grunt.initConfig({
+		express: {
+			options: {
+				// Override defaults here
+			},
+			dev: {
+				options: {
+					script: 'bin/www'
+				}
+			}
+		},
+		watch: {
+			express: {
+				files:  [ '**/*.js' ],
+				tasks:  [ 'express:dev' ],
+				options: {
+					spawn: false // for grunt-contrib-watch v0.5.0+, "nospawn: true" for lower versions. Without this option specified express won't be reloaded
+				}
+			}
+		}
+	});
+
+	// Load plugins
+	grunt.loadNpmTasks('grunt-express-server');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	// Create custom tasks
+	grunt.registerTask('server:dev', [ 'express:dev', 'watch' ])
+};
